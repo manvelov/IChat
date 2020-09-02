384f59dd9673dcd727ad2b2d55bcc54cb075a657
Kirill Manvelov <kmanvelov@gmail.com>
2020-09-02 19:18:43 -0400

    Initial Commit

---
+++ IChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IChat
+//
+//  Created by Kirill Manvelov on 02.09.2020.
+//  Copyright © 2020 Kirill Manvelov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
